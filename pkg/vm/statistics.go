52f61632671a96b4c67e58f6564e685e0fd489cd
Teemu Koponen <koponen@styra.com>
2022-12-01 12:01:40 -0800

    pkg/vm: Drop unnecessary statistics.
    Have in the struct only the fields that are populated.

--- pkg/vm/statistics.go
+++ pkg/vm/statistics.go
@@ -2,7 +2,6 @@ package vm
 
 import (
 	"context"
-	"time"
 )
 
 type (
@@ -13,21 +12,7 @@ var statisticsKey = contextKey("statistics")
 
 type (
 	Statistics struct {
-		BundleReadBytes             int64         `json:"bundle_read_bytes"`
-		BundleReadOps               int64         `json:"bundle_read_ops"`
-		BundleReadTime              time.Duration `json:"bundle_read_time"`
-		ConfigReadBytes             int64         `json:"config_read_bytes"`
-		ConfigReadCacheMisses       int64         `json:"config_read_cache_misses"`
-		ConfigReadDecodeCacheMisses int64         `json:"config_read_decode_cache_misses"`
-		ConfigReadOps               int64         `json:"config_read_ops"`
-		ConfigReadTime              time.Duration `json:"config_read_time"`
-		DataReadBytes               int64         `json:"data_read_bytes"`
-		DataReadCacheMisses         int64         `json:"data_read_cache_misses"`
-		DataReadOps                 int64         `json:"data_read_ops"`
-		DataReadTime                time.Duration `json:"data_read_time"`
-		EvalInstructions            int64         `json:"eval_instructions"`
-		EvalOps                     int64         `json:"eval_ops"`
-		EvalTime                    time.Duration `json:"eval_time"`
+		EvalInstructions int64 `json:"eval_instructions"`
 	}
 )
 
