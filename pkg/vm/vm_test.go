a7fa8fb27b662088de95db876ab4895b3c697d0d
kevinstyra <83973046+kevinstyra@users.noreply.github.com>
2023-01-04 10:18:54 -0500

    pkg/vm: add vm_test unit-test (#105)
    STY-0

--- pkg/vm/vm_test.go
+++ pkg/vm/vm_test.go
@@ -1,1 +1,64 @@
 package vm
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/bundle"
+	"github.com/open-policy-agent/opa/compile"
+	"github.com/open-policy-agent/opa/ir"
+)
+
+func setup(tb testing.TB) ir.Policy {
+	b := &bundle.Bundle{
+		Modules: []bundle.ModuleFile{
+			{
+				URL:    "/url",
+				Path:   "/foo.rego",
+				Raw:    []byte("package test\nallow := x {\nx := true}"),
+				Parsed: ast.MustParseModule("package test\nallow := x {\nx := true}"),
+			},
+		},
+	}
+
+	compiler := compile.New().WithTarget(compile.TargetPlan).WithBundle(b).WithEntrypoints("test/allow")
+	if err := compiler.Build(context.Background()); err != nil {
+		tb.Fatal(err)
+	}
+
+	bundle := compiler.Bundle()
+	var policy ir.Policy
+
+	if err := json.Unmarshal(bundle.PlanModules[0].Raw, &policy); err != nil {
+		tb.Fatal(err)
+	}
+	return policy
+}
+
+func testCompiler(tb testing.TB, policy ir.Policy) {
+	ops := &testDataOperations{}
+	_, err := NewCompiler(ops).WithPolicy(&policy).Compile()
+	if err != nil {
+		tb.Fatal(err)
+	}
+}
+
+type testDataOperations struct {
+	DataOperations
+}
+
+func TestCompiler(t *testing.T) {
+	policy := setup(t)
+	testCompiler(t, policy)
+}
+
+func BenchmarkVm(b *testing.B) {
+	policy := setup(b)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		testCompiler(b, policy)
+	}
+}
