3b5a5640bbbb6ebd9dfa8c065e4c691923830a63
Stephan Renatus <stephan@styra.com>
2023-02-02 09:12:28 +0100

    cmd: "load load convert" -> "load bundle convert" (#184)
    Fixes #179.
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- cmd/load.go
+++ cmd/load.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 func Load() *cobra.Command {
 	return &cobra.Command{
-		Use:   "load",
-		Short: "Styra Load specific commands",
+		Use:   "bundle",
+		Short: "Styra Load Bundle commands",
 	}
 }
