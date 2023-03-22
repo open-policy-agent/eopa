663b798bea149f32a9840233ae16819300611c01
kevinstyra <83973046+kevinstyra@users.noreply.github.com>
2023-03-22 09:55:11 -0400

    add instruction-limit option to 'run' (#405)
    STY-0

--- cmd/command.go
+++ cmd/command.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/open-policy-agent/opa/cmd"
 	"github.com/spf13/cobra"
+
+	"github.com/styrainc/load-private/pkg/rego_vm"
 )
 
 const brand = "Load"
@@ -15,20 +17,29 @@ func addLicenseFlags(c *cobra.Command, key *string, token *string) {
 	c.Flags().StringVar(token, "license-token", "", "Location of file containing STYRA_LOAD_LICENSE_TOKEN")
 }
 
+func addInstructionLimitFlag(c *cobra.Command, instrLimit *int64) {
+	c.Flags().Int64Var(instrLimit, "instruction-limit", 100_000_000, "set instruction limit for VM")
+}
+
 func LoadCommand(license *License) *cobra.Command {
 	var key string
 	var token string
+	var instructionLimit int64
 
 	root := &cobra.Command{
 		Use:   path.Base(os.Args[0]),
 		Short: "Styra Load",
 
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			if license == nil {
-				return
-			}
 			switch cmd.CalledAs() {
 			case "eval", "run":
+				if instructionLimit > 0 {
+					rego_vm.SetLimits(instructionLimit)
+				}
+
+				if license == nil {
+					return
+				}
 				go func() {
 					// do the license validate and activate asynchronously; so user doesn't have to wait
 					license.ValidateLicense(key, token, func(code int, err error) { os.Exit(code) })
@@ -43,12 +54,12 @@ func LoadCommand(license *License) *cobra.Command {
 		switch c.Name() {
 		case "run":
 			addLicenseFlags(c, &key, &token)
+			addInstructionLimitFlag(c, &instructionLimit)
 			root.AddCommand(initRun(c, brand)) // wrap OPA run
 		case "eval":
 			addLicenseFlags(c, &key, &token)
-			var evalInstrLimit int64
-			c.Flags().Int64Var(&evalInstrLimit, "instruction-limit", 100_000_000, "set instruction limit for VM")
-			root.AddCommand(evalWrap(c, &evalInstrLimit))
+			addInstructionLimitFlag(c, &instructionLimit)
+			root.AddCommand(c)
 		case "version":
 			root.AddCommand(initVersion()) // override version
 		default:
