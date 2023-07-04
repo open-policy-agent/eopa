2a1d1bf1695e54d9597af406dd29618a0e0ed7a9
Stephan Renatus <stephan@styra.com>
2023-07-04 14:51:59 +0200

    e2e: retry flaky CLI `eopa eval` test (#778)
    * e2e/retry: lend neat package from hashicorp/consul's tests
    
    https://github.com/hashicorp/consul/blob/sdk/v0.14.0/sdk/testutil/retry/retry.go
    MPL-2.0 licensed
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>
    
    * e2e/cli: wrap two flaky tests in 'retry.Run'
    
    This is a bit of a lame solution, but since these tests randomly fail
    ONLY ON GITHUB, there's not that much we can do; unless skipping them.
    
    Let's try this for a bit.
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>
    
    ---------
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- e2e/cli/license_test.go
+++ e2e/cli/license_test.go
@@ -6,48 +6,54 @@ import (
 	"os/exec"
 	"strings"
 	"testing"
+
+	"github.com/styrainc/enterprise-opa-private/e2e/retry"
 )
 
 func TestNoLicenseForEval(t *testing.T) {
-	// If we run `eopa eval` without a license from env vars/CLI args, it'll fail
-	// and suggest starting a trial. Since the license check happens asynchronously
-	// -- to not stall `eopa eval` calls -- bad licenses can go unnoticed with quick
-	// evals (see test below).
-	eopa := eopaEvalQuery("true")
-	eopa.Env = filter(eopa.Env)
-	stdout, err := eopa.Output()
-	if err == nil {
-		t.Fatal("expected error, got nil")
-	}
-	if len(stdout) > 0 {
-		t.Errorf("expected empty stdout, got %s", stdout)
-	}
-	if e, ok := err.(interface{ ExitCode() int }); ok {
-		if exp, act := 3, e.ExitCode(); exp != act {
-			t.Errorf("expected exit code %d, got %d", exp, act)
+	retry.Run(t, func(r *retry.R) {
+		// If we run `eopa eval` without a license from env vars/CLI args, it'll fail
+		// and suggest starting a trial. Since the license check happens asynchronously
+		// -- to not stall `eopa eval` calls -- bad licenses can go unnoticed with quick
+		// evals (see test below).
+		eopa := eopaEvalQuery("true")
+		eopa.Env = filter(eopa.Env)
+		stdout, err := eopa.Output()
+		if err == nil {
+			t.Fatal("expected error, got nil")
 		}
-	}
-	ee := err.(*exec.ExitError)
-	prefix := `invalid license: no license provided
+		if len(stdout) > 0 {
+			t.Errorf("expected empty stdout, got %s", stdout)
+		}
+		if e, ok := err.(interface{ ExitCode() int }); ok {
+			if exp, act := 3, e.ExitCode(); exp != act {
+				t.Errorf("expected exit code %d, got %d", exp, act)
+			}
+		}
+		ee := err.(*exec.ExitError)
+		prefix := `invalid license: no license provided
 
 Sign up for a free trial now`
-	if act := string(ee.Stderr); !strings.HasPrefix(act, prefix) {
-		t.Errorf("expected output %s, got %s", prefix, act)
-	}
+		if act := string(ee.Stderr); !strings.HasPrefix(act, prefix) {
+			t.Errorf("expected output %s, got %s", prefix, act)
+		}
+	})
 }
 
 func TestNoLicenseForQuickEval(t *testing.T) {
-	// Bad license keys can go unnoticed for quick evals. See comment
-	// above.
-	eopa := eopaEvalQuery("true")
-	eopa.Env = append(filter(eopa.Env), "EOPA_LICENSE_KEY=invalid")
-	stdout, err := eopa.Output()
-	if err != nil {
-		t.Fatalf("expected no error, got %v", err)
-	}
-	if exp, act := "true\n", string(stdout); exp != act {
-		t.Errorf("expected stdout %s, got %s", exp, act)
-	}
+	retry.Run(t, func(r *retry.R) {
+		// Bad license keys can go unnoticed for quick evals. See comment
+		// above.
+		eopa := eopaEvalQuery("true")
+		eopa.Env = append(filter(eopa.Env), "EOPA_LICENSE_KEY=invalid")
+		stdout, err := eopa.Output()
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+		if exp, act := "true\n", string(stdout); exp != act {
+			t.Errorf("expected stdout %s, got %s", exp, act)
+		}
+	})
 }
 
 func TestNoLicenseForLongEval(t *testing.T) {
