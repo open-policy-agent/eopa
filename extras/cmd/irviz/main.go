59f2c701cda48b0122336802095e23fb7bd852cb
Stephan Renatus <stephan@styra.com>
2023-11-02 18:12:16 +0100

    extras/irviz: add plain output (#1143)
    * irviz: add plain output
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>
    
    * irviz: replace | in plain mode
    
    This could help diff utils to actually find moved assignments: the nesting
    no longer matters that much.
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>
    
    ---------
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- extras/cmd/irviz/main.go
+++ extras/cmd/irviz/main.go
@@ -13,8 +13,10 @@ import (
 
 func main() {
 	var filename string
+	var plain bool
 	fs := flag.NewFlagSet("irviz", flag.ExitOnError)
 	fs.StringVar(&filename, "f", "", "IR JSON filename to read in and dump a Graphviz DOT diagram for. (default: stdin)")
+	fs.BoolVar(&plain, "p", false, "plain text output of golang representation")
 	fs.Parse(os.Args[1:])
 
 	// Get input Rego file from stdin or a file on disk.
@@ -38,6 +40,21 @@ func main() {
 	if err := json.Unmarshal(fileBytes.Bytes(), &policy); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+
+	if plain {
+		out := bytes.Buffer{}
+		if err := ir.Pretty(&out, &policy); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		scanner := bufio.NewScanner(&out)
+		for scanner.Scan() {
+			os.Stdout.Write(bytes.ReplaceAll(scanner.Bytes(), []byte("|"), []byte(" ")))
+			os.Stdout.Write([]byte("\n"))
+		}
+		return
 	}
 
 	f, err := PolicyToCFGDAGForest(&policy)
