53f695440ee884cd0d01773fc9e92a8573fa7e62
Philip Conrad <philipaconrad@gmail.com>
2023-11-18 16:18:01 -0500

    pkg/convert: Add delta bundle test.
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>

--- pkg/convert/bundle_test.go
+++ pkg/convert/bundle_test.go
@@ -14,84 +14,124 @@ import (
 )
 
 func TestConvert(t *testing.T) {
-	b := bundle.Bundle{
-		Data: map[string]interface{}{
-			"foo": map[string]interface{}{
-				"bar": []interface{}{json.Number("1"), json.Number("2"), json.Number("3")},
-				"baz": true,
-				"qux": "hello",
+	type testcase struct {
+		note   string
+		bundle bundle.Bundle
+	}
+	testcases := []testcase{
+		{
+			note: "normal snapshot bundle",
+			bundle: bundle.Bundle{
+				Data: map[string]interface{}{
+					"foo": map[string]interface{}{
+						"bar": []interface{}{json.Number("1"), json.Number("2"), json.Number("3")},
+						"baz": true,
+						"qux": "hello",
+					},
+				},
+				Modules: []bundle.ModuleFile{
+					{
+						URL:    "/foo/corge/corge.rego",
+						Path:   "/foo/corge/corge.rego",
+						Parsed: ast.MustParseModule(`package foo.corge`),
+						Raw:    []byte("package foo.corge\n"),
+					},
+				},
+				Manifest: bundle.Manifest{
+					Roots:    &[]string{""},
+					Revision: "quickbrownfaux",
+					Metadata: map[string]interface{}{"version": "v1", "hello": "world"},
+				},
 			},
 		},
-		Modules: []bundle.ModuleFile{
-			{
-				URL:    "/foo/corge/corge.rego",
-				Path:   "/foo/corge/corge.rego",
-				Parsed: ast.MustParseModule(`package foo.corge`),
-				Raw:    []byte("package foo.corge\n"),
+		{
+			note: "delta bundle",
+			bundle: bundle.Bundle{
+				Data: map[string]interface{}{},
+				Patch: bundle.Patch{Data: []bundle.PatchOperation{
+					{Op: "upsert", Path: "/a/b/d", Value: "foo"},
+					{Op: "remove", Path: "/a/b/c"},
+				}},
+				Manifest: bundle.Manifest{
+					Roots:    &[]string{""},
+					Revision: "quickbrownfaux",
+					Metadata: map[string]interface{}{"version": "v1", "hello": "world"},
+				},
 			},
 		},
-		Manifest: bundle.Manifest{
-			Roots:    &[]string{""},
-			Revision: "quickbrownfaux",
-			Metadata: map[string]interface{}{"version": "v1", "hello": "world"},
-		},
 	}
 
-	dir := t.TempDir()
+	for _, tc := range testcases {
+		t.Run(tc.note, func(t *testing.T) {
+			b := tc.bundle
+			dir := t.TempDir()
 
-	inFile := dir + "/in.tar.gz"
-	outFile := dir + "/out.tar.gz"
+			inFile := dir + "/in.tar.gz"
+			outFile := dir + "/out.tar.gz"
 
-	// create source file
-	ifile, err := os.Create(inFile)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err := bundle.NewWriter(ifile).Write(b); err != nil {
-		t.Fatal(err)
-	}
-	ifile.Close()
-
-	// invoke the convert function
-	if err := BundleFile(inFile, outFile); err != nil {
-		t.Fatal(err)
-	}
-
-	// validate the result
-	ofile, err := os.OpenFile(outFile, os.O_RDONLY, 0755)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer ofile.Close()
-
-	b2, err := bundle.NewReader(ofile).WithLazyLoadingMode(true).Read()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if diff := cmp.Diff(b.Manifest, b2.Manifest); diff != "" {
-		t.Errorf("Diff (-want +got):\n%s", diff)
-	}
-
-	if diff := cmp.Diff(b.Modules, b2.Modules); diff != "" {
-		t.Errorf("Diff (-want +got):\n%s", diff)
-	}
-
-	for _, v := range b2.Raw {
-		if v.Path == "/data.json" {
-			if len(v.Value) == 0 {
-				t.Error("Missing bjson value")
-			}
-			res, err := bjson.NewFromBinary(v.Value)
+			// create source file
+			ifile, err := os.Create(inFile)
 			if err != nil {
-				t.Errorf("bjson decode failed: %v, %#v", err, v.Value)
+				t.Fatal(err)
 			}
-			if diff := cmp.Diff(b.Data, res.JSON()); diff != "" {
-				t.Errorf("Diff (-want +got):\n%s", diff)
+
+			if err := bundle.NewWriter(ifile).Write(b); err != nil {
+				t.Fatal(err)
 			}
-			return
-		}
+			ifile.Close()
+
+			// invoke the convert function
+			if err := BundleFile(inFile, outFile); err != nil {
+				t.Fatal(err)
+			}
+
+			// validate the result
+			ofile, err := os.OpenFile(outFile, os.O_RDONLY, 0o755)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer ofile.Close()
+
+			b2, err := bundle.NewReader(ofile).WithLazyLoadingMode(true).Read()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(b.Manifest, b2.Manifest); diff != "" {
+				t.Errorf("[%s] Diff (-want +got):\n%s", tc.note, diff)
+			}
+
+			if diff := cmp.Diff(b.Modules, b2.Modules); diff != "" {
+				t.Errorf("[%s] Diff (-want +got):\n%s", tc.note, diff)
+			}
+
+			switch b2.Type() {
+			case bundle.DeltaBundleType:
+				// TODO(philip): For some reason, the b2.Raw field *is not populated* for delta bundle types?
+				if len(b2.Patch.Data) == 0 {
+					t.Errorf("[%s] Missing patches for delta bundle", tc.note)
+				}
+				return
+			case bundle.SnapshotBundleType:
+				for _, v := range b2.Raw {
+					if v.Path == "/data.json" {
+						if len(v.Value) == 0 {
+							t.Errorf("[%s] Missing bjson value", tc.note)
+						}
+						res, err := bjson.NewFromBinary(v.Value)
+						if err != nil {
+							t.Errorf("[%s] bjson decode failed: %v, %#v", tc.note, err, v.Value)
+						}
+						if diff := cmp.Diff(b.Data, res.JSON()); diff != "" {
+							t.Errorf("[%s] Diff (-want +got):\n%s", tc.note, diff)
+						}
+						return
+					}
+				}
+				t.Errorf("[%s] No data.json found: %#v", tc.note, b2)
+			default:
+				t.Errorf("[%s] Unknown bundle type %#v", tc.note, b2)
+			}
+		})
 	}
-	t.Errorf("No data.json found: %#v", b2)
 }
