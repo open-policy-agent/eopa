7661bd0623153130cb6a3a42de41aabb4da20a1f
Philip Conrad <philipaconrad@gmail.com>
2024-03-19 12:19:35 -0400

    ci: Add Regal version badge auto-updating system.
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>

---
+++ build/get-regal-version.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Script to get Regal version from go.mod file. The script removes the
+# leading 'v' in the release tag. Example v0.8.0 -> 0.8.0.
+# The script also trims whitespaces.
+go list -m -json github.com/styrainc/regal | opa eval -fraw -I 'trim(input.Version, " v")'
