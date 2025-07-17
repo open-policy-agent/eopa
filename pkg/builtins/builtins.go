adf187196b54eeb78f42b3f422bfb0a7b4dccc9f
Stephan Renatus <stephan@styra.com>
2025-07-17 11:55:05 +0200

    builtins: remove superfluous caps update for builtins
    This is already handled by the Init() method above.
    
    The rest of the updateCaps stuff can go once we rebase onto a
    release/branch containing 47f764e5be2818e77f81d10e0dc4888216318e24.
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- pkg/builtins/builtins.go
+++ pkg/builtins/builtins.go
@@ -436,7 +436,6 @@ func Init() {
 func enterpriseOPAExtensions(f *ast.Capabilities) {
 	features := strings.Split("bjson_bundle,grpc_service,kafka_data_plugin,git_data_plugin,ldap_data_plugin,s3_data_plugin,okta_data_plugin,http_data_plugin,lia_plugin", ",")
 	f.Features = append(f.Features, features...)
-	f.Builtins = append(f.Builtins, Builtins...)
 }
 
 func docs(u string, extra ...string) []string {
