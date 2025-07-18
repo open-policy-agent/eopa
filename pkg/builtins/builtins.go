e3992bb19834d2f4c5daa543ff5c877e99792c05
Philip Conrad <philip@chariot-chaser.net>
2025-07-18 19:38:16 -0400

    pkg/builtins: Switch to ast.RegisterFeatures()
    Signed-off-by: Philip Conrad <philip@chariot-chaser.net>

--- pkg/builtins/builtins.go
+++ pkg/builtins/builtins.go
@@ -429,13 +429,9 @@ func Init() {
 		for name, fn := range builtinFunctions {
 			topdown.RegisterBuiltinFunc(name, fn)
 		}
-		updateCaps()
+		features := strings.Split("bjson_bundle,grpc_service,kafka_data_plugin,git_data_plugin,ldap_data_plugin,s3_data_plugin,okta_data_plugin,http_data_plugin,lia_plugin", ",")
+		ast.RegisterFeatures(features...)
 	})
-}
-
-func enterpriseOPAExtensions(f *ast.Capabilities) {
-	features := strings.Split("bjson_bundle,grpc_service,kafka_data_plugin,git_data_plugin,ldap_data_plugin,s3_data_plugin,okta_data_plugin,http_data_plugin,lia_plugin", ",")
-	f.Features = append(f.Features, features...)
 }
 
 func docs(u string, extra ...string) []string {
