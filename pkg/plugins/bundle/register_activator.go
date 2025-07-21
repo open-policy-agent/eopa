32d5e7b2bd8d76616a26ecde71b89857c2e609db
Philip Conrad <philip@chariot-chaser.net>
2025-07-21 16:44:47 -0400

    pkg/plugins/bundle: Add back "default bundle activator" behavior.
    When I refactored the `RegisterActivator` utility function, I had
    forgotten that the `bundle.RegisterActivator` function had *different
    behavior* from our OPA Fork's old version when it was upstreamed.
    
    The fix was calling `RegisterDefaultBundleActivator`, which gets us back
    to the "use EOPA's bundle activator" behavior we had before.
    
    Signed-off-by: Philip Conrad <philip@chariot-chaser.net>

--- pkg/plugins/bundle/register_activator.go
+++ pkg/plugins/bundle/register_activator.go
@@ -6,6 +6,9 @@ import (
 	bundleApi "github.com/open-policy-agent/opa/v1/bundle"
 )
 
+// Ensure our custom bundle activator is available, then set it to be the
+// default for all bundle activations.
 func RegisterActivator() {
 	bundleApi.RegisterActivator("_enterprise_opa", &CustomActivator{})
+	bundleApi.RegisterDefaultBundleActivator("_enterprise_opa")
 }
