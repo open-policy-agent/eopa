050ad0122f155d54672d165fb3968234cacef14d
Stephan Renatus <stephan@styra.com>
2025-07-25 12:55:10 +0200

    e2e/compile: ignore version in DL

--- e2e/compile/logs_test.go
+++ e2e/compile/logs_test.go
@@ -46,7 +46,15 @@ var standardLabels = payloadLabels{
 	Version: os.Getenv("EOPA_VERSION"),
 }
 
-var stdIgnores = cmpopts.IgnoreFields(payload{}, "Timestamp", "Metrics", "DecisionID", "Labels.ID", "NDBC", "Intermediate")
+var stdIgnores = cmpopts.IgnoreFields(payload{},
+	"Timestamp",
+	"Metrics",
+	"DecisionID",
+	"Labels.ID",
+	"NDBC",
+	"Intermediate",
+	"Version",
+)
 
 func TestDecisionLogsCompileAPIResult(t *testing.T) {
 	policy := `
