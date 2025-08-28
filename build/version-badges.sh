90a86447f3be25b8b3238569371720852a143bc0
Stephan Renatus <stephan.renatus@gmail.com>
2025-08-28 15:33:02 +0200

    README: update version, and version-patch script
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- build/version-badges.sh
+++ build/version-badges.sh
@@ -3,10 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -eou pipefail
-REGAL=$(go list -m github.com/styrainc/regal | cut -d' ' -f2)
+REGAL=$(go list -m github.com/open-policy-agent/regal | cut -d' ' -f2)
 OPA=$(go list -m github.com/open-policy-agent/opa | cut -d' ' -f2)
 
 cat <<EOF
 [![OPA ${OPA}](https://img.shields.io/endpoint?url=https://openpolicyagent.org/badge-endpoint/${OPA})](https://github.com/open-policy-agent/opa/releases/tag/${OPA})
-[![Regal ${REGAL}](https://img.shields.io/github/v/release/styrainc/regal?filter=${REGAL}&label=Regal)](https://github.com/StyraInc/regal/releases/tag/${REGAL})
+[![Regal ${REGAL}](https://img.shields.io/github/v/release/open-policy-agent/regal?filter=${REGAL}&label=Regal)](https://github.com/open-policy-agent/regal/releases/tag/${REGAL})
 EOF
