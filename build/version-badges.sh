709f131d9af77957df10a27ef5c9ff2ff83144fb
Stephan Renatus <stephan.renatus@gmail.com>
2025-09-29 08:34:47 +0200

    Release v1.44.0
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- build/version-badges.sh
+++ build/version-badges.sh
@@ -7,6 +7,6 @@ REGAL=$(go list -m github.com/open-policy-agent/regal | cut -d' ' -f2)
 OPA=$(go list -m github.com/open-policy-agent/opa | cut -d' ' -f2)
 
 cat <<EOF
-[![OPA ${OPA}](https://img.shields.io/endpoint?url=https://openpolicyagent.org/badge-endpoint/${OPA})](https://github.com/open-policy-agent/opa/releases/tag/${OPA})
+[![OPA ${OPA}](https://openpolicyagent.org/badge/${OPA})](https://github.com/open-policy-agent/opa/releases/tag/${OPA})
 [![Regal ${REGAL}](https://img.shields.io/github/v/release/open-policy-agent/regal?filter=${REGAL}&label=Regal)](https://github.com/open-policy-agent/regal/releases/tag/${REGAL})
 EOF
